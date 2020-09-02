d6a8c56af24985c8a401b3f60165ec5c4d4541c6
gurbuzertunga <gurbuzertunga@gmail.com>
2020-09-02 13:08:07 +0300

    added test cases for slack-ruby-bot commands

--- bin/main.rb
+++ bin/main.rb
@@ -3,12 +3,3 @@ require_relative '../lib/commands/get_wiki.rb'
 require_relative '../lib/bot.rb'
 
 ## For RSpec Testing Only ##
-class TestCases
-  def add(num, num2)
-    num + num2
-  end
-
-  def multiply(num, num2)
-    num * num2
-  end
-end
--- spec/rspec_testing.rb
+++ spec/rspec_testing.rb
@@ -1,17 +1,54 @@
-require_relative '../bin/main'
+require 'rspec/expectations'
 
-describe TestCases do
-  describe '#add' do
-    it 'returns the sum of two numbers' do
-      testing = TestCases.new
-      expect(testing.add(5, 2)).to eql(7)
+RSpec::Matchers.define :respond_with_slack_message do |expected|
+  include SlackRubyBot::SpecHelpers
+
+  match do |actual|
+    client = respond_to?(:client) ? send(:client) : SlackRubyBot::Client.new
+
+    message_command = SlackRubyBot::Hooks::Message.new
+    channel, user, message, attachments = parse(actual)
+
+    allow(client).to receive(:message) do |options|
+      @messages ||= []
+      @messages.push options
     end
+
+    message_command.call(client, Hashie::Mash.new(text: message, channel: channel, user: user, attachments: attachments))
+
+    matcher = have_received(:message).once
+    matcher = matcher.with(hash_including(channel: channel, text: expected)) if channel && expected
+
+    expect(client).to matcher
+
+    true
   end
 
-  describe '#multiply' do
-    it 'returns the multiplication of two numbers' do
-      testing = TestCases.new
-      expect(testing.multiply(3, 3)).to eql(9)
-    end
+  failure_message do |_actual|
+    message = "expected to receive message with text: #{expected} once,\n received:"
+    message += @messages&.any? ? @messages.inspect : 'none'
+    message
+  end
+end
+
+describe SlackRubyBot::Commands do
+  it 'returns wiki page for ruby strings' do
+    expect(message: "#{SlackRubyBot.config.user} strings").to respond_with_slack_message
+  end
+
+  it 'returns wiki page for ruby arrays' do
+    expect(message: "#{SlackRubyBot.config.user} arrays").to respond_with_slack_message
+  end
+
+  it 'returns wiki page for ruby blocks_and_iterators' do
+    expect(message: "#{SlackRubyBot.config.user} blocks_and_iterators").to respond_with_slack_message
+  end
+
+  it 'returns wiki page for ruby classes' do
+    expect(message: "#{SlackRubyBot.config.user} classes").to respond_with_slack_message
+  end
+
+  it 'says hello' do
+    expect(message: "#{SlackRubyBot.config.user} say_hello").to respond_with_slack_message
   end
 end
